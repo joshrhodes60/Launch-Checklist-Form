cfcc3c491fdb609daa2f79ca5167d4caba885faf
“josh” <joshrhodes60@icloud.com>
2021-03-27 19:27:40 -0500

    part one

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 // Write your JavaScript code here!
+window.addEventListener("load", function () {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function (event) {
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoMass = document.querySelector("input[name=cargoMass]");
+      if (pilotName.value === "" || copilotName.value === "") {
+         alert("All fields are required!");
+         // stop the form submission
+         event.preventDefault();
+      } else if (fuelLevel.value === "" || cargoMass.value === "") {
+         alert("All fields are required!");
+         // stop the form submission
+         event.preventDefault();
+      };
+   });
+});   
+  
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+
+// /* This block of code shows how to format the HTML once you fetch some planetary JSON!
+// <h2>Mission Destination</h2>
+// <ol>
+//    <li>Name: ${}</li>
+//    <li>Diameter: ${}</li>
+//    <li>Star: ${}</li>
+//    <li>Distance from Earth: ${}</li>
+//    <li>Number of Moons: ${}</li>
+// </ol>
+// <img src="${}">
+// */
